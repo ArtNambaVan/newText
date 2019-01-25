c30c8b56a0007d05841f68befd5b02f9cd81b71b
Artem Klochkov <artem.klochkov@soft-werke.com>
2019-01-25 17:41:00 +0300

    add styles forr pop up

--- html/js/main.js
+++ html/js/main.js
@@ -16,9 +16,74 @@ $( document ).ready(function() {
         $(this).closest($tonalityPopup).removeClass('show');
     })
 
-    // $('.dropdown-toggle').on('click', function (event) {
-    //     $(this).siblings('.dropdown-menu').toggleClass('show');
-    // });
+    $('li.dropdown.mega-dropdown a').on('click', function (event) {
+        $(this).parent().toggleClass('open');
+    });
+
+    $('#source').on('click', function (e) {
+        var target = $(e.target);
+        if(target.hasClass('jstree-anchor')){
+            return true; 
+        } else {
+            return false;
+        }
+    });
+
+    $('#source').jstree({
+		'core' : {
+			'data' : [
+				{
+					"text" : "first root node",
+                    "state" : { "opened" : true },
+                    "a_attr" : {
+                        "class" : "jstree-rating"
+                    },
+					"children" : [
+                        { "text" : "Child node 1",
+                            "a_attr" : {
+                                "class" : "jstree-rating"
+                            },
+                         },
+
+                         { "text" : "Child node 2"
+                         },
+
+                         { "text" : "Child node 3"
+                         },
+
+                         { "text" : "Child node 4"
+                         },
+
+                         { "text" : "Child node 5",
+                            "a_attr" : {
+                                "class" : "jstree-rating"
+                            },
+                         },
+
+						{ "text" : "Child node 6"}
+					]
+                },
+                {
+					"text" : "second root node",
+					"state" : { "opened" : true },
+					"children" : [
+						{
+							"text" : "Child node 1",
+							"state" : { "opened" : true },
+                            "icon" : "jstree-file",
+                            "children" : [
+                                {
+                                    "text" : "Child node 11"
+                                },
+                                { "text" : "Child node 22"}
+                            ]
+						},
+						{ "text" : "Child node 2"}
+					]
+				}
+			]
+		}
+    });
 
 
 
